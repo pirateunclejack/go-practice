7d442690ae60271c7fe5749f7a6e0857e823e1ff
pirateunclejack <17961436+pirateunclejack@users.noreply.github.com>
2024-01-24 01:56:46 +0800

    go-react-calorie-tracker-yt lesson2

--- go-react-calorie-tracker-yt/server/routes/entries.go
+++ go-react-calorie-tracker-yt/server/routes/entries.go
@@ -1,5 +1,88 @@
 package routes
 
-func AddEntry()  {
-	
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
+
+func AddEntry(c *gin.Context)  {
+
+}
+
+func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err := cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+}
+
+func GetEntriesByIngredient(c *gin.Context) {
+
+}
+
+func GetEntryById(c *gin.Context) {
+	EntryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(EntryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var entry bson.M
+	if err := entryCollection.FindOne(ctx, bson.M{"_id": docID}).Decode(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(entry)
+	c.JSON(http.StatusOK, entry)
+}
+
+func UpdateEntry(c *gin.Context) {
+
+}
+
+func UpdateIngredient(c *gin.Context) {
+
+}
+
+func DeleteEntry(c *gin.Context) {
+	entryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(entryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := entryCollection.DeleteOne(ctx, bson.M{"_id": docID}) 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, result.DeletedCount)
 }
